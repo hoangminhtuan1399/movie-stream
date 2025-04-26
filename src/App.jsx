c9778929d617ef90286e12f37526b5fee2e045a5
net <hoangminhtuan1399@gmail.com>
2025-04-26 20:19:59 +0700

    add admin nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from "react-router-dom";
+import { MoviePage } from "./pages/admin/MoviePage.jsx";
+import { CollectionPage } from "./pages/admin/CollectionPage.jsx";
+import { FeaturePage } from "./pages/admin/FeaturePage.jsx";
+import { ActorPage } from "./pages/admin/ActorPage.jsx";
+import { ReportPage } from "./pages/admin/ReportPage.jsx";
+import AdminLayout from "./pages/admin/AdminLayout.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path={'/admin'} element={<AdminLayout />}>
+        <Route index element={<MoviePage/>}/>
+        <Route path={'movie'} element={<MoviePage/>}/>
+        <Route path={'collection'} element={<CollectionPage/>}/>
+        <Route path={'feature'} element={<FeaturePage/>}/>
+        <Route path={'actor'} element={<ActorPage/>}/>
+        <Route path={'report'} element={<ReportPage/>}/>
+      </Route>
+    </Routes>
   )
 }
 
