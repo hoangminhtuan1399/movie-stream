a6088360529cfbcfc92477226fdf72eba1a544c5
bao long <pikalong0311@gmail.com>
2025-05-08 20:09:56 +0700

    create component topic card

---
+++ src/pages/user/HomePage/dummyData.jsx
@@ -0,0 +1,8 @@
+export const topicList = [
+    { title: 'Marvel', href: '/c/marvel', bgColor: '#2D5BFF' },
+    { title: '4K', href: '/c/4k', bgColor: '#6C6993' },
+    { title: 'Sitcom', href: '/c/sitcom', bgColor: '#288A73' },
+    { title: 'Lồng Tiếng Cực Mạnh', href: '/c/long-tieng', bgColor: '#8A6EDB' },
+    { title: 'Xuyên Không', href: '/c/xuyen-khong', bgColor: '#CF7753' },
+    { title: 'Cổ Trang', href: '/c/co-trang', bgColor: '#A73939' },
+  ];
